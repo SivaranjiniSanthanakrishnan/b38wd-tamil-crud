304e6d3ea95fe6d82c44527f07671ec12ce29d14
SivaranjiniSanthanakrishnan <43466009+SivaranjiniSanthanakrishnan@users.noreply.github.com>
2022-09-15 12:52:05 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// API Call => Response
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div style={{ paddingLeft: "20px" }}>
+    <App
+      componentName="App Functional Component"
+      componentType="Functional Component"
+      response={{ name: "XXX", age: 20 }}
+    />
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
